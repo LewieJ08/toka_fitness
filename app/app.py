8f426b6dafd7d7648b8d448608dc712ead025bd6
LewieJ08 <lewiej08@gmail.com>
2025-06-07 01:12:50 +0100

    feat: add init guest routes

--- app/app.py
+++ app/app.py
@@ -7,5 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about", methods=["GET","POST"])
+
+def about():
+    return render_template("about.html")
+
+@app.route("/articles", methods=["GET","POST"])
+
+def articles():
+    return render_template("articles.html")
+
+@app.route("/login", methods=["GET","POST"])
+
+def login():
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET","POST"])
+
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
